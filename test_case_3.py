af3fb121e0c7efb7a994879c67144937a59063e7
ProstakovAlexey <alexey@tulalinux.ru>
2016-04-19 16:41:53 +0300

    Добавлены два теста для выгрузки с выбором не выгружать заявления по детям

--- test_case_3.py
+++ test_case_3.py
@@ -104,6 +104,10 @@ def delLK(DB):
 
 
 class case3(unittest.TestCase):
+    """Проверяет выгрузку в ЛК и работы веб-сервиса Jaminai. Сначала проверит работу галочек,
+    потом будет выгружать за разные периоды и проверять ответы веб-сервиса. Попутно проверит протоколирование выгрузки
+    и обращения к сервису на ТИ.
+    Всю выгрузку проводит в район Энск (ИД=17), перед выполнением очищает данные для района в ЛК."""
 
     @staticmethod
     def setUpClass():
@@ -156,7 +160,7 @@ class case3(unittest.TestCase):
                 # если выбран, снимаю
                 checkBox.click()
 
-
+    #@unittest.skip('временно отключен')
     def test_1_load_2013(self):
         """ Выгружает ПКУ для ЛК за 2013г.
         :return:
@@ -190,9 +194,10 @@ class case3(unittest.TestCase):
         p.click()
         p.send_keys('31122013')
         # убираю все галочки с выплат
-        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf')
+        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf', 'ctl00_cph_CB_nopay')
         self.clearCheck(driver, checkIdList)
         # ставлю галочки
+        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf')
         for id in checkIdList:
             driver.find_element_by_id(id).click()
         # проверяю работу галочки не выгружать закрытые признаки учета
@@ -236,7 +241,7 @@ class case3(unittest.TestCase):
                          driver.find_element_by_id("ctl00_cph_L_Res").text,
                          'Передача ПКУ для ЛК прошла не успешно')
 
-
+    #@unittest.skip('временно отключен')
     def test_2(self):
         """Направляет запрос к сервису Jaminai для проверки после загрузки данных за 2013г. Проверяет результат
         на соответствие образцу 111-111-221 45_1.xml. Кратко: за 2013г. - 2650 руб."""
@@ -252,8 +257,8 @@ class case3(unittest.TestCase):
             summ = parseJam(result)
             self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого в тесте №4')
 
-
-    def test_3(self):
+    #@unittest.skip('временно отключен')
+    def test_3_load(self):
         """ Выгружает ПКУ для ЛК за 2014г."""
         driver = self.driver
         driver.get(self.base_url + "VisitingService/ViewGosUsl.aspx")
@@ -284,9 +289,10 @@ class case3(unittest.TestCase):
         p.click()
         p.send_keys('31122014')
         # убираю все галочки с выплат
-        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf')
+        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf', 'ctl00_cph_CB_nopay')
         self.clearCheck(driver, checkIdList)
         # ставлю галочки
+        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf')
         for id in checkIdList:
             driver.find_element_by_id(id).click()
         # проверяю работу галочки не выгружать закрытые признаки учета
@@ -328,7 +334,7 @@ class case3(unittest.TestCase):
                          driver.find_element_by_id("ctl00_cph_L_Res").text,
                          'Передача ПКУ для ЛК прошла не успешно')
 
-
+    #@unittest.skip('временно отключен')
     def test_4(self):
         """ Направляет запрос к сервису Jaminai для проверки после загрузки данных за весь период"""
         IS = TI
@@ -343,7 +349,7 @@ class case3(unittest.TestCase):
             summ = parseJam(result)
             self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого в тесте №4')
 
-
+    #@unittest.skip('временно отключен')
     def test_5(self):
         """ Направляет запрос к сервису Jaminai только за 2013г. для проверки после загрузки данных"""
         IS = TI
@@ -358,7 +364,7 @@ class case3(unittest.TestCase):
             summ = parseJam(result)
             self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого в тесте №4')
 
-
+    #@unittest.skip('временно отключен')
     def test_6(self):
         """ Направляет запрос к сервису Jaminai только за 2014г. для проверки после загрузки данных должно быть 0"""
         IS = TI
@@ -373,7 +379,7 @@ class case3(unittest.TestCase):
             summ = parseJam(result)
             self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого')
 
-
+    #@unittest.skip('временно отключен')
     def test_7(self):
         """ Направляет запрос к сервису Jaminai только за 2015г."""
         IS = TI
@@ -387,6 +393,97 @@ class case3(unittest.TestCase):
                 f.write(result)
             summ = parseJam(result)
             self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого')
+
+    #@unittest.skip('временно отключен')
+    def test_8_load(self):
+        """Повторит выгрузку за 2013-2015г. но укажет, чтобы не выгружалось по заявлениям = Заявка на ежемесячное пособие на детей.
+        Выгрузка должна перетереть имеющиеся данные"""
+
+        driver = self.driver
+        driver.get(self.base_url + "VisitingService/ViewGosUsl.aspx")
+        # захожу в выгрузку на ТИ
+        driver.find_element_by_id("ctl00_cph_lbtnExport").click()
+        # снимаю галочки для выгрузки всего
+        checkIdList = ('ctl00_cph_CB_PKU', 'ctl00_cph_CB_State', 'ctl00_cph_CB_SMEV', 'ctl00_cph_CB_PKU_SMEV')
+        self.clearCheck(driver, checkIdList)
+        # ставлю галочку на ПКУ для ЛК
+        driver.find_element_by_id('ctl00_cph_CB_PKU').click()
+        time.sleep(2)
+        # снимаю галочки на выгрузку всего, если они стояли
+        checkIdList = ('ctl00_cph_CB_payments', 'ctl00_cph_CB_pu', 'ctl00_cph_CB_yd',
+                       'ctl00_cph_CB_payinfo', 'ctl00_cph_CB_msp')
+        self.clearCheck(driver, checkIdList)
+        # ставлю на ПКУ с ПИН кодом
+        driver.find_element_by_id('ctl00_cph_RBL_Type_PKU_1').click()
+        # ставлю не выгружать персональную информацию
+        driver.find_element_by_id('ctl00_cph_CB_Pers').click()
+        # ставлю выплаты
+        driver.find_element_by_id('ctl00_cph_CB_payments').click()
+        # установливаю период с
+        p = driver.find_element_by_id('igtxtctl00_cph_periodDateFrom')
+        p.click()
+        p.send_keys('01012013')
+        # установить дату период по
+        p = driver.find_element_by_id('igtxtctl00_cph_periodDateTo')
+        p.click()
+        p.send_keys('31122015')
+        # убираю все галочки с выплат
+        checkIdList = ('ctl00_cph_CB_unpaid', 'ctl00_cph_CB_negative', 'ctl00_cph_CB_Inf', 'ctl00_cph_CB_nopay')
+        self.clearCheck(driver, checkIdList)
+        # ставлю галочки, в т.ч. на Не выгружать информацию по мерам соцподдержки
+        for id in checkIdList:
+            driver.find_element_by_id(id).click()
+        # проверяю работу галочки не выгружать закрытые признаки учета
+        p = driver.find_element_by_id('ctl00_cph_CB_closed_pu')
+        self.assertEqual(p.is_displayed(), True,
+                         'Галочка признаки учета должна быть не активна при не выбранной Признаки учета')
+        # проверяю галочку не выгружать закрытые удостоверения
+        p = driver.find_element_by_id('ctl00_cph_CB_no_yd')
+        self.assertEqual(p.is_displayed(), True, 'Галочка не выгружать удостоверения закрытые более года назад должна '
+                                                 'быть disable при не выбранной Удостоверения')
+        # устанавливаю галочку Признаки учета
+        driver.find_element_by_id('ctl00_cph_CB_pu').click()
+        # проверяю работу галочки не выгружать закрытые признаки учета
+        # тут ошибка в ПО, поэтому пока закомментировал
+        p = driver.find_element_by_id('ctl00_cph_CB_closed_pu')
+        self.assertEqual(p.is_displayed(), True, 'Галочка признаки учета должна быть активна при выбранной Признаки учета')
+        # запускает отправка в ЛК
+        driver.find_element_by_id('ctl00_cph_UltraWebTab1__ctl1_lbSendFilePortal').click()
+        # ждет, когда отработает прогресс бар (30 мин)
+        for i in range(60):
+            try:
+                p = driver.find_element_by_css_selector("b > b")
+                break
+            except:
+                pass
+        else:
+            self.fail("Не дождался окончания выгрузки ПКУ в ЛК")
+        # проверяет, что прогрессбар успешно завершился
+        s = driver.page_source
+        if s.find('Операция завершена') == -1:
+            self.fail('После загрузки ПКУ в ЛК возникла ошибка на прогрессбаре')
+        # нажать на ОК на прогрессбаре
+        p.click()
+        # проверяет, что выгрузка прошла успешно
+        self.assertEqual("1. Соединение с ИТ. ОК\n2. Передача информации на ИТ ЛК. OK",
+                         driver.find_element_by_id("ctl00_cph_L_Res").text,
+                         'Передача ПКУ для ЛК прошла не успешно')
+
+    #@unittest.skip('временно отключен')
+    def test_9(self):
+        """Направить запрос на весь период после выгрузки без учета заявлений Заявка на ежемесячное пособие на детей.
+        Сравнит с контрольной суммой"""
+        IS = TI
+        # заполняет словарь со СНИСЛ и файлами образцами ответов
+        snilsDict = {'111-111-221 45': 2007.0}
+        # перебирает тестовые СНИЛС
+        for snils in snilsDict.keys():
+            # направляет запрос в сервис по СНИЛС
+            result = sendJam(IS, snils)
+            with open('Результаты/' + snils + '_9.xml', encoding='utf-8', mode='w') as f:
+                f.write(result)
+            summ = parseJam(result)
+            self.assertEqual(summ, snilsDict[snils], 'Не совпадает сумма итого в тесте №9')
 
 
     def is_element_present(self, how, what):
